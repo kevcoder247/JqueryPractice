da5895fd3221ceb94ece0e38ae3c296c9a6dde76
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-05-17 01:47:23 -0400

    basic javascript add that allows us to take the text/value of the input and add it to our list

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('hello')
+//Create a on click event that captures the input value when the add skill button is clicked
+$("#addSkill").on('click', () => {
+  //create a variable that stores the value of the input 
+  const inputValue = $('#myInput').val();
+  //take the value of the input and append it to the button of  the skills list
+  $("#mySkills").append(inputValue);
+  console.log(inputValue)
+})
+
+
