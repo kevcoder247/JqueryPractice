29c78bb92e77485416263a27bb20510688e8899e
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-05-17 13:01:38 -0400

    event listener added that allows user to add append new skill to the skill list

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
+//create a refernece variable that hold or makes a call to the UL where we will send the input value when add skill is clicked
+const $skillsList = $("#mySkills");
+//console.log($skillsList)
+
 //Create a on click event that captures the input value when the add skill button is clicked
 $("#addSkill").on('click', () => {
   //create a variable that stores the value of the input 
-  const inputValue = $('#myInput').val();
-  //take the value of the input and append it to the button of  the skills list
-  $("#mySkills").append(inputValue);
-  console.log(inputValue)
+  let $inputValue = $("#myInput").val();
+
+  //Create a new list item with the value of what is typed in the input field
+  const $newSkill = `<li>${$inputValue}</li>`
+  
+  //take the value of the input and append it to the skills list
+  $($skillsList).append($newSkill);
+  console.log($newSkill)
+  //$inputValue.value = '';
+
+  $("#myInput").val('');
+  
 })
+
+
+
 
 
