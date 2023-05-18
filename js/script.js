74cc1ae34a32ee4f703a07e9b46c910bbcd85588
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-05-18 11:44:57 -0400

    add event listener added so that the skill is deleted when clicked

--- js/script.js
+++ js/script.js
@@ -8,16 +8,32 @@ $("#addSkill").on('click', () => {
   let $inputValue = $("#myInput").val();
 
   //Create a new list item with the value of what is typed in the input field
-  const $newSkill = `<li>${$inputValue}</li>`
+  const $newSkill = $(`<li><i class="fa-solid fa-rectangle-xmark x"></i>${$inputValue}</li>`);
   
   //take the value of the input and append it to the skills list
   $($skillsList).append($newSkill);
   console.log($newSkill)
-  //$inputValue.value = '';
 
   $("#myInput").val('');
-  
+
+  //Add event listener that removes skill when its clicked on(later we will updated to add a icon we can click)
+  $($newSkill).on('click', function() {
+    $(this).remove()
+    console.log(this)
+  })
+
+
+
+  ///change the above event listener so that wheen the icon is clicked the li is deleted
 })
+
+//removed main heading when its clicked
+$("#mainHeading").on('click', function() {
+  $(this).remove();
+})
+
+
+
 
 
 
